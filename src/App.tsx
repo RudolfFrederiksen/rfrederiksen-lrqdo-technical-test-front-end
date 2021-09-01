54abeefc685e9b000dc7986c1faf642525a195eb
RudolfFrederiksen <rudolf.frederiksen@gmail.com>
2021-09-01 23:31:21 +0200

    setup search field with debounce

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import "./App.scss";
 import ProductList from "./components/Product-list/ProductList";
-import Loader from "./components/Loader/Loader";
 
 function App() {
     return (
         <div className="App">
             <ProductList />
-
-            <Loader />
         </div>
     );
 }
--- src/components/Product-list/ProductList.tsx
+++ src/components/Product-list/ProductList.tsx
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import styles from "./ProductList.module.scss";
+import Loader from "../Loader/Loader";
+import debounce from "lodash.debounce";
 
-const ProductList = () => (
-    <div className={styles.ProductList} data-testid="Product-list">
-        Product-list Component
-    </div>
-);
+export function ProductList() {
+    const [search, setSearch] = useState("");
+    const [error, setError] = useState("");
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [products, setProducts] = useState([]);
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(event.target.value);
+        setIsLoaded(false);
+
+        // todo: load products
+    };
+    const debouncedChangeHandler = useMemo(() => debounce(handleSearchChange, 300), []);
+
+    const renderError = () => {
+        // const { error, isLoaded, products } = this.state;
+        return <div>Error: {error}</div>;
+    };
+
+    const renderLoader = () => {
+        // const { error, isLoaded, products } = this.state;
+        return <Loader />;
+    };
+
+    const renderProducts = () => {
+        // const { error, isLoaded, products } = this.state;
+        return products.length > 0 ? (
+            // render product list
+            <div className={styles.ProductList} data-testid="Product-list">
+                Product-list Component
+                {/*
+                                  <ul>
+                    {items.map(item => (
+                        <li key={item.id}>
+                            {item.name} {item.price}
+                        </li>
+                    ))}
+                </ul>
+                  */}
+            </div>
+        ) : (
+            // no search
+            <div>Type a keyword in the search field to load matching food</div>
+        );
+    };
+    return (
+        <div>
+            <input type="text" onChange={debouncedChangeHandler} />
+            {error ? renderError() : isLoaded ? renderProducts() : renderLoader()}
+        </div>
+    );
+}
 
 export default ProductList;
