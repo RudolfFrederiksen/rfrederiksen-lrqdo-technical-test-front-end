aec317e6028ab4821f541df461a8b91023dff33c
RudolfFrederiksen <rudolf.frederiksen@gmail.com>
2021-09-02 01:23:33 +0200

    load products with keyword add interfaces update favicon and header data style application

--- src/components/Product-list/ProductList.tsx
+++ src/components/Product-list/ProductList.tsx
@@ -1,22 +1,61 @@
-import React, { useMemo, useState } from "react";
-import styles from "./ProductList.module.scss";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import "./ProductList.scss";
 import Loader from "../Loader/Loader";
 import debounce from "lodash.debounce";
+import { Product, ProductResult } from "../interfaces/Products.interfaces";
 
 export function ProductList() {
     const [search, setSearch] = useState("");
     const [error, setError] = useState("");
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [products, setProducts] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(true);
+    const [products, setProducts] = useState<Array<Product> | []>([]);
 
+    // debounce search input
     const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setSearch(event.target.value);
+        setError("");
         setIsLoaded(false);
-
-        // todo: load products
+        setSearch(event.target.value);
     };
     const debouncedChangeHandler = useMemo(() => debounce(handleSearchChange, 300), []);
+    // cancel debounce on unmount
+    useEffect(() => {
+        return () => {
+            debouncedChangeHandler.cancel();
+        };
+    });
 
+    // load products when search is updated
+    const loadProducts = useCallback(() => {
+        fetch(
+            `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${search}&search_simple=1&action=process&json=1`
+        )
+            .then((res) => res.json())
+            .then(
+                (result: ProductResult) => {
+                    setIsLoaded(true);
+                    setProducts(result.products);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setError("An error was raised during product load. Please retry");
+                }
+            );
+    }, [search]);
+    useEffect(() => {
+        if (search) {
+            loadProducts();
+        } else {
+            // reset product list, if we don't have anything to search for
+            setProducts([]);
+            setIsLoaded(true);
+        }
+    }, [search, loadProducts]);
+
+    /*
+     * RENDER ZONE
+     */
     const renderError = () => {
         // const { error, isLoaded, products } = this.state;
         return <div>Error: {error}</div>;
@@ -31,26 +70,41 @@ export function ProductList() {
         // const { error, isLoaded, products } = this.state;
         return products.length > 0 ? (
             // render product list
-            <div className={styles.ProductList} data-testid="Product-list">
-                Product-list Component
-                {/*
-                                  <ul>
-                    {items.map(item => (
-                        <li key={item.id}>
-                            {item.name} {item.price}
-                        </li>
+            <table>
+                <thead>
+                    <tr>
+                        <th className="product-img">image</th>
+                        <th>name</th>
+                        <th>allergens</th>
+                        <th>official product page</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product) => (
+                        <tr key={product._id}>
+                            <td className="product-img">
+                                <img src={product.image_url} alt={product.product_name} />
+                            </td>
+                            <td>{product.product_name}</td>
+                            <td>{product.allergens}</td>
+                            <td>
+                                <a href={product.link} target="_blank" rel="noreferrer">
+                                    {product.link}
+                                </a>
+                            </td>
+                        </tr>
                     ))}
-                </ul>
-                  */}
-            </div>
+                </tbody>
+            </table>
         ) : (
             // no search
             <div>Type a keyword in the search field to load matching food</div>
         );
     };
+
     return (
-        <div>
-            <input type="text" onChange={debouncedChangeHandler} />
+        <div className="ProductList" data-testid="Product-list">
+            <input type="text" onChange={debouncedChangeHandler} placeholder="Search a product" />
             {error ? renderError() : isLoaded ? renderProducts() : renderLoader()}
         </div>
     );
---
+++ src/components/interfaces/Products.interfaces.ts
@@ -0,0 +1,15 @@
+export interface ProductResult {
+    count: number;
+    page: number;
+    page_count: number;
+    page_size: number;
+    products: Array<Product>;
+}
+
+export interface Product {
+    _id: number;
+    product_name: string;
+    allergens: string;
+    image_url: string;
+    link: string;
+}
